efc68a2eca75d61eb69bb84e4c906cc72e6c4579
Yevhen Chementsov <yevhenchementsov@gmail.com>
2024-10-16 12:16:57 +0300

    Add aliases with default import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      components: resolve(__dirname, 'src/components'),
+      data: resolve(__dirname, 'src/data'),
+    },
+  },
 });
